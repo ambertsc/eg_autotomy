406119b3645aaa897879e65e002cb28292c66755
RiveSunder <rivesunder@protonmail.com>
2022-09-02 14:57:49 -0600

    remove dead code (envs/__init__.py) and extra line (.coveragerc)
    per code review suggestions

--- envs/__init__.py
+++ envs/__init__.py
@@ -5,14 +5,6 @@ from envs.back_and_forth_env import BackAndForthEnvClass
 
 # register the env using gym's interface
 
-"""
-register(
-    id = 'AdaptiveWalkEnv-v0',
-    entry_point = 'envs.adaptive_walk_env:AdaptiveWalkEnvClass',
-    max_episode_steps = 1024
-)
-"""
-
 register(
     id = 'BackAndForthEnv-v0',
     entry_point = 'envs.back_and_forth_env:BackAndForthEnvClass',

90e9173784d7015999405c03b025bf8a17f953c0
RiveSunder <rivesunder@protonmail.com>
2022-09-02 12:40:10 -0600

    add test framework

---
+++ eg_tests/test_check_connected.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestCheckConnected(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_check_connected(self):
+
+        result = False
+        self.assertTrue(result)
+
+if __name__ == "__name__":
+
+    unittest.main(verbosity=2)

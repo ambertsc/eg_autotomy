e9de50659d07ffc8b00972a3ef01ddc3c0459b25
RiveSunder <RiveSunder@protonmail.com>
2022-09-29 14:24:54 -0600

    add save_gif to parse args in tests for enjoy

--- eg_tests/test_enjoy.py
+++ eg_tests/test_enjoy.py
@@ -23,24 +23,30 @@ class TestEnjoy(unittest.TestCase):
     def test_no_render(self):
         parser = argparse.ArgumentParser("Experiment parameters")
 
-        parser.add_argument("-n", "--env_name", type=str, \
-                help="name of environemt", default="BackAndForthEnv-v0")
-        parser.add_argument("-pi", "--policy", type=str,\
-                help="name of policy architecture", default="MLPPolicy")
-        parser.add_argument("-e", "--episodes", type=int,\
-                help="number of episodes", default=1)
-        parser.add_argument("-s", "--save_frames", type=bool, \
-                help="save frames or not", default=False)
-        parser.add_argument("-nr", "--no_render", type=bool,\
-                help="don't render", default=True)
-        parser.add_argument("-ms", "--max_steps", type=int,\
-                help="maximum number of steps per episode", default=10)
-        parser.add_argument("-f", "--file_path", type=str,\
-                help="file path to model parameters", \
-                default="./results/test_exp/")
         parser.add_argument("-a", "--num_agents", type=int,\
                 help="how many agents to evaluate", \
                 default=1)
+        parser.add_argument("-e", "--episodes", type=int,\
+                help="number of episodes", default=5)
+        parser.add_argument("-f", "--file_path", type=str,\
+                help="file path to model parameters", \
+                default="./results/test_exp/")
+        parser.add_argument("-g", "--save_gif", type=float, default=0,\
+                help="1 - save gif to ./assets, 0 - do not.") 
+        parser.add_argument("-m", "--mode", default=0,\
+                help="mode (0,1,2, or 3) for body co-evolution")
+        parser.add_argument("-ms", "--max_steps", type=int,\
+                help="maximum number of steps per episode", default=4000)
+        parser.add_argument("-n", "--env_name", type=str, \
+                help="name of environemt", default="InvertedPendulumBulletEnv-v0")
+        parser.add_argument("-nr", "--no_render", type=bool,\
+                help="don't render", default=False)
+        parser.add_argument("-pi", "--policy", type=str,\
+                help="name of policy architecture", default="MLPPolicy")
+        parser.add_argument("-s", "--save_frames", type=bool, \
+                help="save frames or not", default=False)
+        parser.add_argument("-u", "--use_autotomy", type=int, default=1,\
+                help="allow autotomy in training (for envs that support it)")
 
         args = parser.parse_args()
 
@@ -51,27 +57,28 @@ class TestEnjoy(unittest.TestCase):
     def test_no_render_body(self):
         parser = argparse.ArgumentParser("Experiment parameters")
 
-        parser.add_argument("-n", "--env_name", type=str, \
-                help="name of environemt", default="BackAndForthEnv-v0")
-        parser.add_argument("-pi", "--policy", type=str,\
-                help="name of policy architecture", default="MLPBodyPolicy")
-        parser.add_argument("-e", "--episodes", type=int,\
-                help="number of episodes", default=1)
-        parser.add_argument("-s", "--save_frames", type=bool, \
-                help="save frames or not", default=False)
-        parser.add_argument("-nr", "--no_render", type=bool,\
-                help="don't render", default=True)
-        parser.add_argument("-ms", "--max_steps", type=int,\
-                help="maximum number of steps per episode", default=10)
-        parser.add_argument("-f", "--file_path", type=str,\
-                help="file path to model parameters", \
-                default="./results/test_exp/")
         parser.add_argument("-a", "--num_agents", type=int,\
                 help="how many agents to evaluate", \
                 default=1)
-        parser.add_argument("-b", "--body_dim", type=int,\
-                help="body dim", \
-                default=8)
+        parser.add_argument("-e", "--episodes", type=int,\
+                help="number of episodes", default=5)
+        parser.add_argument("-f", "--file_path", type=str,\
+                help="file path to model parameters", \
+                default="./results/test_exp/")
+        parser.add_argument("-g", "--save_gif", type=float, default=0,\
+                help="1 - save gif to ./assets, 0 - do not.") 
+        parser.add_argument("-m", "--mode", default=0,\
+                help="mode (0,1,2, or 3) for body co-evolution")
+        parser.add_argument("-ms", "--max_steps", type=int,\
+                help="maximum number of steps per episode", default=4000)
+        parser.add_argument("-n", "--env_name", type=str, \
+                help="name of environemt", default="InvertedPendulumBulletEnv-v0")
+        parser.add_argument("-nr", "--no_render", type=bool,\
+                help="don't render", default=False)
+        parser.add_argument("-pi", "--policy", type=str,\
+                help="name of policy architecture", default="MLPPolicy")
+        parser.add_argument("-s", "--save_frames", type=bool, \
+                help="save frames or not", default=False)
         parser.add_argument("-u", "--use_autotomy", type=int, default=1,\
                 help="allow autotomy in training (for envs that support it)")
 
